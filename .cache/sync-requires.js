ec639b9881b9cd9fbf3d74b2c3cb62fbc9e7bb3e
Gear61 <chessnone@yahoo.com>
2020-11-22 19:30:33 -0800

    New images

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/alexanderchiou/web/lion-brothers/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/alexanderchiou/web/lion-brothers/.cache/dev-404-page.js"))),
   "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("/Users/alexanderchiou/web/lion-brothers/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")))
 }
 
